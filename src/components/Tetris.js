8b04062b5f958c70127bbdc89d23ecbd9c8cf72e
Ian <ian@macbook-pro.lan>
2020-07-27 12:21:53 -0500

    added hook files for player, stage, and game status

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,6 +6,7 @@ import { createStage } from '../gameHelpers';
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 const Tetris = () => {
+    console.log(createStage());
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
